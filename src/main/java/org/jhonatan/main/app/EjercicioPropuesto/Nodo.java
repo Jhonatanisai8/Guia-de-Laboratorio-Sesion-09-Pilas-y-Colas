5cb43998daf6b18dc2dc7ba9ec9701e779a7cee0
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-06-13 12:28:26 -0500

    ejercicio propuesto: clase nodo atributos y constructor

---
+++ src/main/java/org/jhonatan/main/app/EjercicioPropuesto/Nodo.java
@@ -0,0 +1,12 @@
+package org.jhonatan.main.app.EjercicioPropuesto;
+
+public class Nodo {
+    private Empleado empleado;
+    Nodo sig;
+
+    public Nodo(Empleado empleado) {
+        this.empleado = empleado;
+        this.sig = null;
+    }
+
+}
