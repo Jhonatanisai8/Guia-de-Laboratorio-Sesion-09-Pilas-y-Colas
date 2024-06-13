ad538efb9339b842d37e0152f44885e0466cf114
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-06-13 12:44:20 -0500

    Ejercicio Propuesto: clase pila atributos, metodo buscar y mensaje del empleado eliminado

---
+++ src/main/java/org/jhonatan/main/app/EjercicioPropuesto/Pila.java
@@ -0,0 +1,54 @@
+package org.jhonatan.main.app.EjercicioPropuesto;
+
+import java.util.StringTokenizer;
+
+import javax.swing.JOptionPane;
+
+public class Pila {
+    // variables
+    Nodo tope;
+    Nodo pfound;
+
+    int num = 0;
+
+    public Pila() {
+        this.tope = null;
+    }
+
+    public Nodo buscar(Nodo tope,String nombres){
+        Nodo pos = tope;
+        while (pos != null !nombres.equalsIgnoreCase(pos.getEmpleado().getNombre())) {
+            pos = pos.sig;
+        }
+
+        //retornamos la posicion
+        return pos;
+    }
+
+    public void mensaje(String data){
+        StringTokenizer st = new StringTokenizer(data, ",");
+        //partiendo el texto
+        String nombre = st.nextToken();
+        String aPaterno = st.nextToken();
+        String aMaterno = st.nextToken();
+        String sueldoBase = st.nextToken();
+        String ventasRealizadas = st.nextToken();
+        String estadoCivil = st.nextToken();
+        String numHijos = st.nextToken();
+        String sueldoNeto = st.nextToken();
+
+        String datos =  "Descripcion del dato Eliminado: \n"+
+        "Nombres:       "+nombre+"\n"+
+        "Ap. Paterno:   "+aPaterno+"\n"+
+        "Ap. Materno:   "+aMaterno+"\n"+
+        "Sueldo Base:   "+sueldoBase+"\n"+
+        "Ven.Realizadas:"+ventasRealizadas+"\n"+
+        "Estado Civil:  "+estadoCivil+"\n"+
+        "Nº de hijos:   "+numHijos+"\n"+
+        "Sueldo Neto:   "+sueldoNeto+"\n";
+
+        JOptionPane.showMessageDialog(null, datos,"ATENCIÓN",JOptionPane.INFORMATION_MESSAGE);
+        
+    }
+
+}
