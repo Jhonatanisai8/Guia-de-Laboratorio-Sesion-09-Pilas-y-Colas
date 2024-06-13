712a7590f4eb9cdc4153109666ee49c87aa69d5e
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-06-13 12:11:00 -0500

    clase empleado: Atributos

---
+++ src/main/java/org/jhonatan/main/app/EjercicioPropuesto/Empleado.java
@@ -0,0 +1,14 @@
+package org.jhonatan.main.app.EjercicioPropuesto;
+
+public class Empleado {
+    // atributos
+    private String nombre;
+    private String aPaterno;
+    private String aMaterno;
+    private float sueldoBase;
+    private int ventasRelizadas;
+    private String estadoCivil;
+    private int numeroHijos;
+    private float sueldoNeto;
+
+}
