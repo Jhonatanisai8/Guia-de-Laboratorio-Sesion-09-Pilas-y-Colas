b53066b78019f76009730b6711f517626cb8324e
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-06-12 22:01:42 -0500

    Agregación de depencias al archivo pom.xml y iconos para los botonos ademas de un método en la clase main para llamar al formaluario con la versión de pilas y hacerlo visible

--- src/main/java/org/jhonatan/main/app/Sesion09RegistroEmpleados.java
+++ src/main/java/org/jhonatan/main/app/Sesion09RegistroEmpleados.java
@@ -1,5 +1,7 @@
 package org.jhonatan.main.app;
 
+import org.jhonatan.main.app.formularios.frmRegistrosPilas;
+
 /**
  *
  * @author Jhonatan
@@ -7,6 +9,11 @@ package org.jhonatan.main.app;
 public class Sesion09RegistroEmpleados {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        formularioPilas();
+    }
+
+    public static void formularioPilas() {
+        frmRegistrosPilas d = new frmRegistrosPilas();
+        d.setVisible(true);
     }
 }
