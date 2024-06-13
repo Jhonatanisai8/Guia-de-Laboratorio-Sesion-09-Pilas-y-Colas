9a96cf8cf56e3172a6f03d36c597e9bc63c2b183
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-06-12 21:31:01 -0500

    CREACIÓN DEL PROYECTO

---
+++ src/main/java/org/jhonatan/main/app/Sesion09RegistroEmpleados.java
@@ -0,0 +1,12 @@
+package org.jhonatan.main.app;
+
+/**
+ *
+ * @author Jhonatan
+ */
+public class Sesion09RegistroEmpleados {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
