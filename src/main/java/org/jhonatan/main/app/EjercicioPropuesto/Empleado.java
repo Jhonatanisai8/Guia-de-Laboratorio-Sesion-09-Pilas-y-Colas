a3d7399d43f3167ca2b24848459f6fd10db455d5
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-06-13 12:12:36 -0500

    clase empleado: constructor y getters,setters

--- src/main/java/org/jhonatan/main/app/EjercicioPropuesto/Empleado.java
+++ src/main/java/org/jhonatan/main/app/EjercicioPropuesto/Empleado.java
@@ -11,4 +11,81 @@ public class Empleado {
     private int numeroHijos;
     private float sueldoNeto;
 
+    public Empleado(String nombre, String aPaterno, String aMaterno, float sueldoBase, int ventasRelizadas,
+            String estadoCivil, int numeroHijos) {
+        this.nombre = nombre;
+        this.aPaterno = aPaterno;
+        this.aMaterno = aMaterno;
+        this.sueldoBase = sueldoBase;
+        this.ventasRelizadas = ventasRelizadas;
+        this.estadoCivil = estadoCivil;
+        this.numeroHijos = numeroHijos;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getaPaterno() {
+        return aPaterno;
+    }
+
+    public void setaPaterno(String aPaterno) {
+        this.aPaterno = aPaterno;
+    }
+
+    public String getaMaterno() {
+        return aMaterno;
+    }
+
+    public void setaMaterno(String aMaterno) {
+        this.aMaterno = aMaterno;
+    }
+
+    public float getSueldoBase() {
+        return sueldoBase;
+    }
+
+    public void setSueldoBase(float sueldoBase) {
+        this.sueldoBase = sueldoBase;
+    }
+
+    public int getVentasRelizadas() {
+        return ventasRelizadas;
+    }
+
+    public void setVentasRelizadas(int ventasRelizadas) {
+        this.ventasRelizadas = ventasRelizadas;
+    }
+
+    public String getEstadoCivil() {
+        return estadoCivil;
+    }
+
+    public void setEstadoCivil(String estadoCivil) {
+        this.estadoCivil = estadoCivil;
+    }
+
+    public int getNumeroHijos() {
+        return numeroHijos;
+    }
+
+    public void setNumeroHijos(int numeroHijos) {
+        this.numeroHijos = numeroHijos;
+    }
+
+    public float getSueldoNeto() {
+        return sueldoNeto;
+    }
+
+    public void setSueldoNeto(float sueldoNeto) {
+        this.sueldoNeto = sueldoNeto;
+    }
+
+    
+    
 }
