776cf3881709d0ea0df6207e6f0b0fba1ad91e6b
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-06-14 19:06:55 -0500

    ejercicio propuesto version colas: clase nodo metodo para ver los datos del dato empleado eliminado

--- src/main/java/org/jhonatan/main/app/EjercicioPropuestoColas/Cola.java
+++ src/main/java/org/jhonatan/main/app/EjercicioPropuestoColas/Cola.java
@@ -1,5 +1,9 @@
 package org.jhonatan.main.app.EjercicioPropuestoColas;
 
+import java.util.StringTokenizer;
+
+import javax.swing.JOptionPane;
+
 public class Cola {
     // variables
     Nodo frente, finCola;
@@ -19,4 +23,31 @@ public class Cola {
         // retornamos la posicion
         return pos;
     }
+
+    public void mensaje(String data) {
+        StringTokenizer st = new StringTokenizer(data, ",");
+        // partiendo el texto
+        String nombre = st.nextToken();
+        String aPaterno = st.nextToken();
+        String aMaterno = st.nextToken();
+        String sueldoBase = st.nextToken();
+        String ventasRealizadas = st.nextToken();
+        String estadoCivil = st.nextToken();
+        String numHijos = st.nextToken();
+        String sueldoNeto = st.nextToken();
+
+        String datos = "Descripcion del dato Eliminado: \n" +
+                "Nombres:       " + nombre + "\n" +
+                "Ap. Paterno:   " + aPaterno + "\n" +
+                "Ap. Materno:   " + aMaterno + "\n" +
+                "Sueldo Base:   " + sueldoBase + "\n" +
+                "Ven.Realizadas:" + ventasRealizadas + "\n" +
+                "Estado Civil:  " + estadoCivil + "\n" +
+                "Nº de hijos:   " + numHijos + "\n" +
+                "Sueldo Neto:   " + sueldoNeto + "\n";
+
+        JOptionPane.showMessageDialog(null, datos, "ATENCIÓN", JOptionPane.INFORMATION_MESSAGE);
+
+    }
+
 }
